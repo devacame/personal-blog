a0e948d7e8a7d6491b973eab90bdc500f9e3ec8c
DEVACA <154665994+devacame@users.noreply.github.com>
2024-04-15 13:03:56 +0900

    fix(tailwindcss): fixed no defaultTheme set error

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['SpoqaHanSansNeo', ...defaultTheme.fontFamily.sans],
+				sans: ['SpoqaHanSansNeo', ...defaultTheme.fontFamily.sans]
 			}
 		},
 	},
