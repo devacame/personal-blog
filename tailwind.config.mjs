789b2d6aa38ff417fb56a8e079782fb55c4db8d5
DEVACA <154665994+devacame@users.noreply.github.com>
2024-04-17 16:45:07 +0900

    fix(style): fixed theme toggle button not working with tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
+	darkMode: 'selector',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
