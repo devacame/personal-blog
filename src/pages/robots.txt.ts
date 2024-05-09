cf7c9e94780e1bdcb7a78e257561d4197b5af0c8
DEVACA <154665994+devacame@users.noreply.github.com>
2024-05-09 16:09:31 +0900

    feat: added robots.txt

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,67 @@
+import type { APIRoute } from 'astro';
+import { SITE_URL } from '@consts'
+
+const robotsTxt = `
+User-agent: *
+Allow: /
+Disallow: /en/search
+Disallow: /ko/search
+
+User-agent: Amazonbot
+Disallow: /
+
+User-agent: anthropic-ai
+Disallow: /
+
+User-agent: Applebot
+Disallow: /
+
+User-agent: Bytespider
+Disallow: /
+
+User-agent: CCBot
+Disallow: /
+
+User-agent: ChatGPT-User
+Disallow: /
+
+User-agent: Claude-Web
+Disallow: /
+
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: cohere-ai
+Disallow: /
+
+User-agent: Diffbot
+Disallow: /
+
+User-agent: FacebookBot
+Disallow: /
+
+User-agent: Google-Extended
+Disallow: /
+
+User-agent: GPTBot
+Disallow: /
+
+User-agent: omgili
+Disallow: /
+
+User-agent: PerplexityBot
+Disallow: /
+
+User-agent: YouBot
+Disallow: /
+
+Sitemap: ${new URL('sitemap-index.xml', 'https://'+SITE_URL).href}
+`.trim();
+
+export const GET: APIRoute = () => {
+  return new Response(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+};
