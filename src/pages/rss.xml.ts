71b96466d27002a76a13b6ae5db97bf741921b42
DEVACA <154665994+devacame@users.noreply.github.com>
2024-05-12 15:40:47 +0900

    feat: added rss page

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,17 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '@consts'
+import { getPostUrl } from '@utils/postslug'
 
-export async function GET(context) {
-	const posts = await getCollection('blog')
+export async function GET(context: { site: any }) {
+	const posts = (await getCollection('blog')).filter((post) => !post.data.isDraft).sort((a, b) => b.data.pubDate - a.data.pubDate)
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: getPostUrl(post.slug),
 		})),
 	})
 }
