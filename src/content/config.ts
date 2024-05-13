116a955837b2332160af6358a54e2e9aad2d47ca
DEVACA <154665994+devacame@users.noreply.github.com>
2024-05-13 22:24:55 +0900

    fix: use astro content collection recommended image schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,15 @@ import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
 	type: 'content',
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		isDraft: z.boolean(),
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string(),
+		heroImage: image().refine((img) => img.width >= 1080, {
+			message: "Cover image must be at least 1080 pixels wide!",
+		  }),
 		language: z.enum(['en', 'ko']),
 		finishedLanguage: z.enum(['en', 'ko']).optional(),
 		tags: z.array(z.string()),
