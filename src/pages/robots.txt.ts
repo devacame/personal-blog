5899ba2960b1fe135e22a5fcc622be473689ed62
DEVACA <154665994+devacame@users.noreply.github.com>
2024-06-18 11:13:32 +0900

    fix: fixed sites not indexed

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,53 +4,6 @@ import { SITE_URL } from '@consts'
 const robotsTxt = `
 User-agent: *
 Allow: /
-Disallow: /en/search
-Disallow: /ko/search
-
-User-agent: Amazonbot
-Disallow: /
-
-User-agent: anthropic-ai
-Disallow: /
-
-User-agent: Applebot
-Disallow: /
-
-User-agent: Bytespider
-Disallow: /
-
-User-agent: CCBot
-Disallow: /
-
-User-agent: ChatGPT-User
-Disallow: /
-
-User-agent: Claude-Web
-Disallow: /
-
-User-agent: ClaudeBot
-Disallow: /
-
-User-agent: cohere-ai
-Disallow: /
-
-User-agent: Diffbot
-Disallow: /
-
-User-agent: FacebookBot
-Disallow: /
-
-User-agent: GPTBot
-Disallow: /
-
-User-agent: omgili
-Disallow: /
-
-User-agent: PerplexityBot
-Disallow: /
-
-User-agent: YouBot
-Disallow: /
 
 Sitemap: ${new URL('sitemap-index.xml', 'https://' + SITE_URL).href}
 `.trim()
